f3ea819a477db982e1db93e912de7f5eb87f0a31
Ethan Ling <ethanling123@gmail.com>
2020-08-08 11:44:40 -0700

    Inital Projject

---
+++ src/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+
+    private int x = 0;
+    private int y = 0;
+    private String name = "";
+    private boolean present = true;
+
+    public Student(int x, int y, String name, boolean present){
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.present = present;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+
+}
